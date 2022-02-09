c0efdf95e0aed0691c4e75407b3cf309c49bad8e
fininfo\hsoussi <soussih@gmail.com>
2022-02-09 16:34:33 +0100

    log port

--- index.js
+++ index.js
@@ -35,5 +35,5 @@ app.use("/api/orders", orderRoute);
 app.use("/api/checkout", stripeRoute);
 
 app.listen(process.env.PORT || 5000, () => {
-  console.log("Backend server is running!");
+  console.log("Backend server is running!"+process.env.PORT);
 });
