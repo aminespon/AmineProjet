fb72703eb2802899a804643a4076e94729b845f2
fininfo\hsoussi <soussih@gmail.com>
2022-02-10 12:30:12 +0100

    static order

--- index.js
+++ index.js
@@ -23,10 +23,11 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/', function(req, res) {
+
+app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+app.use(express.static(path.join(__dirname, 'build')));
 app.use("/api/auth/", authRoute);
 app.use("/api/users/", userRoute);
 app.use("/api/products/", productRoute);
