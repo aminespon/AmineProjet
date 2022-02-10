760ce191201621d5fb78b30d79f90ce7df6ea8f7
fininfo\hsoussi <soussih@gmail.com>
2022-02-10 11:10:49 +0100

    test route

--- index.js
+++ index.js
@@ -24,15 +24,16 @@ mongoose
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'build')));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 app.use("/api/auth/*", authRoute);
 app.use("/api/users/*", userRoute);
 app.use("/api/products/*", productRoute);
 app.use("/api/carts/*", cartRoute);
 app.use("/api/orders/*", orderRoute);
 app.use("/api/checkout/*", stripeRoute);
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+
 
 
 app.listen(process.env.PORT || 5000, () => {
